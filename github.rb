06aea797f996b2ea6619c6207365a815d228c103
bgpat <bgpat@users.noreply.github.com>
2019-05-28 11:05:58 +0900

    Initial commit

---
+++ github.rb
@@ -0,0 +1,52 @@
+require 'date'
+require 'octokit'
+require './okr'
+
+@client = Octokit::Client.new(:access_token => ENV['GITHUB_ACCESS_TOKEN'])
+@client.auto_paginate = true
+
+today = Date.today
+first = Date.new(today.year, today.month)
+last = Date.new(today.year, today.month).next_month - 1
+
+monthly_results = @client.list_issues('wantedly/infrastructure', {
+  state: :all,
+  creator: ENV['USER'],
+  labels: 'Monthly Result',
+}).map do |issue|
+  "- #{issue.title} ##{issue.number}"
+end
+
+weekly_goal = @client.list_issues('wantedly/infrastructure', {
+  state: :all,
+  labels: 'Weekly Goal',
+  since: first,
+}).map do |issue|
+  "- #{issue.title} ##{issue.number}"
+end
+
+daily_goal = @client.list_issues('wantedly/infrastructure', {
+  state: :all,
+  labels: 'Daily Goal',
+  since: Date.new(today.year, today.month),
+}).map do |issue|
+  "- #{issue.title} ##{issue.number}"
+end
+
+{
+  PREVIOUS: ['<details>', '', *monthly_results, '', '</details>'].join("\n"),
+  'Quarter Goal': format_objectives(get_objectives(view: :owner, page: 1, state: :current)),
+  'Weekly Goal': weekly_goal.join("\n"),
+  'Daily Goal': daily_goal.join("\n"),
+  'GitHub Activities': {
+    ':shipit: マージされた PR': "@wantedly+author:#{ENV['USER']}+merged",
+    ':memo: レビューしたor関わった PR': "@wantedly+-author:#{ENV['USER']}+commenter:#{ENV['USER']}+merged",
+    ':memo: 関わった&解決した Issue': "@wantedly+is:issue+commenter:#{ENV['USER']}+closed",
+    ':memo: 関わった&未解決の Issue': "@wantedly+is:issue+state:open+commenter:#{ENV['USER']}+updated",
+  }.map{|k,v| "- [#{k}](https://github.com/issues?q=#{v}:#{first}..#{last})"}.join("\n"),
+  Description: 'TBD',
+  Comment: 'TBD',
+}.map do |k, v|
+  puts "## #{k}\n\n"
+  puts "#{v}\n\n"
+end
---
+++ okr.rb
@@ -0,0 +1,53 @@
+require 'net/https'
+require 'uri'
+require 'json'
+
+uri = URI('https://my.15five.com/account/login')
+resp = Net::HTTP.post_form(
+  uri,
+  {
+    username: ENV['OKR_USERNAME'],
+    password: ENV['OKR_PASSWORD'],
+  },
+)
+raise resp if resp.code != '302'
+@cookies = resp.get_fields('Set-Cookie').map do |cookie|
+  cookie.split('; ').first
+end
+
+def get_objectives(**params)
+  uri = URI('https://my.15five.com/objectives/api/objectives/')
+  uri.query = URI.encode_www_form(params)
+  http = Net::HTTP.new(uri.host, uri.port)
+  http.use_ssl = true
+  req = Net::HTTP::Get.new(uri)
+  req[:Accept] = 'application/json'
+  req[:Cookie] = @cookies.join('; ')
+  resp = http.request(req)
+  result = JSON.parse(resp.body)['results']
+  result.select { |o| o['marked_completed_ts'].nil? }.map do |o|
+    key_results = o['key_results'].map do |kr|
+      {
+        description: kr['description'],
+      }
+    end
+    children = get_objectives(parent_id: o['id']) if o['children_count'] > 0
+    {
+      id: o['id'],
+      description: o['description'],
+      children: [*key_results, *children],
+    }
+  end
+end
+
+def format_objectives(objectives, indent = 0)
+  spaces = '  ' * indent
+  objectives.map do |o|
+    row = o[:description]
+    row = "[#{row}](https://my.15five.com/objectives/details/#{o[:id]})" if o[:id]
+    [
+      "#{spaces}- #{row}",
+      *format_objectives(o[:children], indent + 1),
+    ]
+  end.join("\n") if objectives
+end
